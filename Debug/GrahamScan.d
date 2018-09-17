5aa1bb29aea880f106779a2a435d6358033f9e8e
Tianyi Zhang <tianyiz@g.clemson.edu>
2018-09-17 03:30:17 -0400

    delete

--- Debug/GrahamScan.d
+++
@@ -1,8 +0,0 @@
-GrahamScan.o: ../GrahamScan.cpp ../GrahamScan.h ../Point.h \
- ../PointStack.h
-
-../GrahamScan.h:
-
-../Point.h:
-
-../PointStack.h:
--- Debug/PointStack.d
+++
@@ -1,5 +0,0 @@
-PointStack.o: ../PointStack.cpp ../PointStack.h ../Point.h
-
-../PointStack.h:
-
-../Point.h:
